95b778221baf17b18f2af688d660a4c73de3bf42
Faiyaz Alam <faiyaz.alam@happiestminds.com>
2018-01-30 19:24:43 +0530

    added timezone field on user profile, order by for csv

---
+++ includes/class-user-login-history-user-profile-helper.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * User_Login_History_DB_Helper
+ * 
+ *
+ * @link       https://github.com/faiyazalam
+ *
+ * @package    User_Login_History
+ * @subpackage User_Login_History/includes
+ * @author     Er Faiyaz Alam
+ */
+class User_Login_History_DB_Helper {
+//
+// define('ULH_MESSAGE_INVALID_NONCE', __('Well tried.', 'user-login-history'));
+//define('ULH_MESSAGE_RECORD_DELETED', __('The record(s) has been deleted.', 'user-login-history'));
+//define('ULH_MESSAGE_NO_RECORD_FOUND', __('No records found.', 'user-login-history'));
+
+    /**
+     * 
+     * @global type $wpdb
+     * @param type $id
+     * @return string
+     */
+    static public function get_blog_name_by_id($id = NULL) {
+        if (!$id) {
+            return '';
+        }
+        global $wpdb;
+        return $wpdb->get_var("SELECT CONCAT(domain,path) AS blog_name FROM $wpdb->blogs WHERE blog_id = $id ");
+    }
+    
+    static public function get_message($key = '') {
+        $messages = array(
+            'invalid_nonce'=>__('Well tried.', 'user-login-history'),
+            'record_deleted'=>__('The record(s) has been deleted.', 'user-login-history'),
+            'no_record_found'=>__('No records found.', 'user-login-history'),
+            );
+        
+            return isset($messages[$key]) ? $messages[$key] : __('Message string is missing.', 'user-login-history');
+    }
+
+    
+    static public function get_table_prefix(){
+         if(is_multisite()){
+             global $wpdb;
+             return  $wpdb->get_blog_prefix(User_Login_History_Session_Helper::get_session_current_login_blog_id());
+        }  else {
+            global $table_prefix;
+            return $table_prefix;
+        }
+    }
+    
+    static function get_table_name() {
+        return self::get_table_prefix(). USER_LOGIN_HISTORY_TABLE_NAME;
+    }
+
+    
+
+    
+    
+
+}
