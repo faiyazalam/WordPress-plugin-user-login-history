acb4ec663ee514c65580b9ab882c65c0cde45f0d
unknown <fyzalam@gmail.com>
2018-08-26 16:50:32 +0530

    added settings basic and advanced

---
+++ inc/admin/class-Settings.php
@@ -0,0 +1,350 @@
+<?php
+
+namespace User_Login_History\Inc\Admin;
+
+use User_Login_History as NS;
+use User_Login_History\Inc\Common\Helpers\DbHelper;
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @link       http://userloginhistory.com
+ * @since      1.0.0
+ *
+ * @author    Er Faiyaz Alam
+ */
+final class LoginListTable extends \User_Login_History\Inc\Common\Abstracts\ListTableAbstract {
+
+    private $table;
+    private $message;
+    private $bulk_action_form;
+    private $delete_action;
+    private $delete_action_nonce;
+    private $bulk_action_nonce;
+
+    public function __construct($plugin_name, $version, $plugin_text_domain) {
+        $args = array(
+            'singular' => $plugin_name . '_user_login', //singular name of the listed records
+            'plural' => $plugin_name . '_user_logins', //plural name of the listed records
+        );
+        parent::__construct($plugin_name, $version, $plugin_text_domain, $args);
+        $this->table = NS\PLUGIN_TABLE_FA_USER_LOGINS;
+        
+        $this->bulk_action_form = $this->_args['singular'] . "_form";
+        $this->delete_action_nonce = $this->_args['singular']."_delete_none";
+        $this->delete_action = $this->_args['singular']."_delete";
+        $this->set_message(esc_html__('Please try again.', $this->plugin_text_domain));
+        $this->bulk_action_nonce = 'bulk-' . $this->_args['plural'];
+    }
+
+    public function get_message() {
+        return $this->message;
+    }
+    
+    public function get_bulk_action_form() {
+        return $this->bulk_action_form;
+    }
+
+    public function set_message($message = '') {
+        $this->message = $message;
+    }
+
+    /**
+     * Prepares the where query.
+     *
+     * @access public
+     * @return string
+     */
+    private function prepare_where_query() {
+
+        $where_query = '';
+
+        $fields = array(
+            'user_id',
+            'username',
+            'browser',
+            'operating_system',
+            'ip_address',
+            'timezone',
+            'country_name',
+            'old_role',
+        );
+
+        foreach ($fields as $field) {
+            if (!empty($_GET[$field])) {
+                $where_query .= " AND `FaUserLogin`.`$field` = '" . esc_sql(trim($_GET[$field])) . "'";
+            }
+        }
+
+        if (!empty($_GET['role'])) {
+            $where_query .= " AND `UserMeta`.`meta_value` LIKE '%" . esc_sql($_GET['role']) . "%'";
+        }
+
+
+        if (!empty($_GET['date_type'])) {
+            $UserProfile = new Faulh_User_Profile($this->plugin_name, NULL);
+            $input_timezone = $UserProfile->get_current_user_timezone();
+            $date_type = $_GET['date_type'];
+            if (in_array($date_type, array('login', 'logout', 'last_seen'))) {
+
+                if (!empty($_GET['date_from']) && !empty($_GET['date_to'])) {
+                    $date_type = esc_sql($date_type);
+                    $date_from = Faulh_Date_Time_Helper::convert_timezone($_GET['date_from'] . " 00:00:00", $input_timezone);
+                    $date_to = Faulh_Date_Time_Helper::convert_timezone($_GET['date_to'] . " 23:59:59", $input_timezone);
+                    $where_query .= " AND `FaUserLogin`.`time_$date_type` >= '" . esc_sql($date_from) . "'";
+                    $where_query .= " AND `FaUserLogin`.`time_$date_type` <= '" . esc_sql($date_to) . "'";
+                } else {
+                    unset($_GET['date_from']);
+                    unset($_GET['date_to']);
+                }
+            }
+        }
+
+
+        if (!empty($_GET['login_status'])) {
+
+            if ("unknown" == $_GET['login_status']) {
+                $where_query .= " AND `FaUserLogin`.`login_status` = '' ";
+            } else {
+                $where_query .= " AND `FaUserLogin`.`login_status` = '" . esc_sql($_GET['login_status']) . "'";
+            }
+        }
+
+        $where_query = apply_filters('faulh_admin_prepare_where_query', $where_query);
+        return $where_query;
+    }
+
+    /**
+     * Retrieve rows
+     * 
+     * @access   public
+     * @param int $per_page
+     * @param int $page_number
+     * @access   public
+     * @return mixed
+     */
+    public function get_rows($per_page = 20, $page_number = 1) {
+        global $wpdb;
+        $table = $wpdb->prefix . $this->table;
+        $sql = " SELECT"
+                . " FaUserLogin.*, "
+                . " UserMeta.meta_value, TIMESTAMPDIFF(SECOND,FaUserLogin.time_login,FaUserLogin.time_last_seen) as duration"
+                . " FROM " . $table . "  AS FaUserLogin"
+                . " LEFT JOIN $wpdb->usermeta AS UserMeta ON ( UserMeta.user_id=FaUserLogin.user_id"
+                . " AND UserMeta.meta_key LIKE  '" . $wpdb->prefix . "capabilities' )"
+                . " WHERE 1 ";
+
+        $where_query = $this->prepare_where_query();
+        if ($where_query) {
+            $sql .= $where_query;
+        }
+        if (!empty($_REQUEST['orderby'])) {
+            $sql .= ' ORDER BY ' . esc_sql($_REQUEST['orderby']);
+            $sql .= !empty($_REQUEST['order']) ? ' ' . esc_sql($_REQUEST['order']) : ' ASC';
+        } else {
+            $sql .= ' ORDER BY id DESC';
+        }
+
+        if ($per_page > 0) {
+            $sql .= " LIMIT $per_page";
+            $sql .= ' OFFSET   ' . ( $page_number - 1 ) * $per_page;
+        }
+
+        return DbHelper::get_results($sql);
+    }
+
+    /**
+     * Returns the count of records in the database.
+     * 
+     * @access   public
+     * @return null|string
+     */
+    public function record_count() {
+        global $wpdb;
+        $table = $wpdb->prefix . $this->table;
+        $sql = " SELECT"
+                . " COUNT(FaUserLogin.id) AS total"
+                . " FROM " . $table . " AS FaUserLogin"
+                . " LEFT JOIN $wpdb->usermeta AS UserMeta ON ( UserMeta.user_id=FaUserLogin.user_id"
+                . " AND UserMeta.meta_key LIKE '" . $wpdb->prefix . "capabilities' )"
+                . " WHERE 1 ";
+
+        $where_query = $this->prepare_where_query();
+
+        if ($where_query) {
+            $sql .= $where_query;
+        }
+
+        return DbHelper::get_var($sql);
+    }
+
+    public function get_columns() {
+        $columns = array(
+            'cb' => '<input type="checkbox" />',
+            'user_id' => esc_html__('User ID', $this->plugin_text_domain),
+            'username' => esc_html__('Username', $this->plugin_text_domain),
+            'role' => esc_html__('Role', $this->plugin_text_domain),
+            'old_role' => esc_html__('Old Role', $this->plugin_text_domain),
+            'browser' => esc_html__('Browser', $this->plugin_text_domain),
+            'operating_system' => esc_html__('Operating System', $this->plugin_text_domain),
+            'ip_address' => esc_html__('IP Address', $this->plugin_text_domain),
+            'timezone' => esc_html__('Timezone', $this->plugin_text_domain),
+            'country_name' => esc_html__('Country', $this->plugin_text_domain),
+            'user_agent' => esc_html__('User Agent', $this->plugin_text_domain),
+            'duration' => esc_html__('Duration', $this->plugin_text_domain),
+            'time_last_seen' => esc_html__('Last Seen', $this->plugin_text_domain),
+            'time_login' => esc_html__('Login', $this->plugin_text_domain),
+            'time_logout' => esc_html__('Logout', $this->plugin_text_domain),
+            'login_status' => esc_html__('Login Status', $this->plugin_text_domain),
+        );
+        return $columns;
+    }
+
+    public function get_sortable_columns() {
+        $columns = array(
+            'user_id' => array('user_id', true),
+            'username' => array('username', true),
+            'old_role' => array('old_role', true),
+            'time_login' => array('time_login', false),
+            'time_logout' => array('time_logout', false),
+            'browser' => array('browser', false),
+            'operating_system' => array('operating_system', false),
+            'country_name' => array('country_name', false),
+            'time_last_seen' => array('time_last_seen', false),
+            'timezone' => array('timezone', false),
+            'user_agent' => array('user_agent', false),
+            'login_status' => array('login_status', false),
+            'is_super_admin' => array('is_super_admin', false),
+            'duration' => array('duration', false),
+        );
+
+        return $columns;
+    }
+
+    /**
+     * Method for name column
+     * 
+     * @access   public
+     * @param array $item an array of DB data
+     * @return string
+     */
+    function column_username($item) {
+
+        $username = $this->is_empty($item['username'])?$this->unknown_symbol : esc_html($item['username']);
+        if ($this->is_empty($item['user_id'])) {
+            $title = $username;
+        } else {
+            $edit_link = get_edit_user_link($item['user_id']);
+            $title = !empty($edit_link) ? "<a href='" . $edit_link . "'>" . $username . "</a>" : '<strong>' . $username . '</strong>';
+        }
+
+        $delete_nonce = wp_create_nonce($this->delete_action_nonce);
+        $actions = array(
+            'delete' => sprintf('<a href="?page=%s&action=%s&record_id=%s&_wpnonce=%s">Delete</a>', esc_attr($_REQUEST['page']), $this->delete_action, absint($item['id']), $delete_nonce),
+        );
+        return $title . $this->row_actions($actions);
+    }
+
+    /**
+     * Returns an associative array containing the bulk action
+     *
+     * @access public 
+     * @return array
+     */
+    public function get_bulk_actions() {
+        $actions = array(
+            'bulk-delete' => esc_html__('Delete Selected Records', 'faulh'),
+            'bulk-delete-all-admin' => esc_html__('Delete All Records', 'faulh'),
+        );
+
+        return $actions;
+    }
+
+    /**
+     * Render the bulk edit checkbox
+     * 
+     * @access   public
+     * @param array $item
+     * @return string
+     */
+    public function column_cb($item) {
+        return sprintf('<input type="checkbox" name="bulk-action-ids[]" value="%s" />', $item['id']);
+    }
+
+    /**
+     * Check form submission and then 
+     * process the bulk operation.
+     * 
+     * @access   public 
+     * @return boolean
+     */
+    public function process_action() {
+        
+        if(empty($_REQUEST['_wpnonce']))
+        {
+            return;
+        }
+        
+        if (isset($_POST[$this->bulk_action_form]) && wp_verify_nonce($_POST['_wpnonce'], $this->bulk_action_nonce)) {
+        return $this->process_bulk_action();
+        }
+
+
+  
+        if (wp_verify_nonce($_GET['_wpnonce'], $this->delete_action_nonce)) {
+        return $this->process_single_action();
+        }
+        
+       
+    }
+    private function process_bulk_action() {
+        switch ($this->current_action()) {
+            case 'bulk-delete':
+                $status = DbHelper::delete_rows_by_table_and_ids($this->table, $_POST['bulk-action-ids']);
+                if ($status) {
+                    $this->set_message( esc_html__('Selected record(s) deleted.', $this->plugin_text_domain));
+                }
+                break;
+            case 'bulk-delete-all-admin':
+                $status = DbHelper::truncate_table($this->table);
+                if ($status) {
+                     $this->set_message(esc_html__('All record(s) deleted.', $this->plugin_text_domain));
+                }
+                break;
+            default:
+                $status = FALSE;
+
+                break;
+        }
+
+        return $status;
+    }
+    
+    private function process_single_action() {
+        if(empty($_GET['record_id']))
+        {
+            return;
+        }
+        
+        $id = absint($_GET['record_id']);
+     
+        switch ($this->current_action()) {
+            case $this->delete_action:
+                $status = DbHelper::delete_rows_by_table_and_ids($this->table, array($id));
+                if ($status) {
+                    $this->set_message( esc_html__('Selected record deleted.', $this->plugin_text_domain));
+                }
+                break;
+
+            default:
+                  $status = FALSE;
+                break;
+        }
+      
+        return $status;
+    }
+
+}
---
+++ inc/common/interfaces/class-IAdminSettings.php
@@ -0,0 +1,303 @@
+<?php
+
+namespace User_Login_History\Inc\Common;
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @link       http://userloginhistory.com
+ * @since      1.0.0
+ *
+ * @author    Er Faiyaz Alam
+ */
+
+    class LoginTracker {
+
+        /**
+         * Login Status Constants.
+         * 
+         */
+        const LOGIN_STATUS_LOGIN = 'login';
+        const LOGIN_STATUS_FAIL = 'fail';
+        const LOGIN_STATUS_LOGOUT = 'logout';
+
+        /**
+         * LOGIN_STATUS_BLOCK
+         * This will be saved in db if user is not allowed to login on another blog.
+         * This is for network enabled mode only.
+         */
+        const LOGIN_STATUS_BLOCK = 'block';
+
+        /**
+         * The unique identifier of this plugin.
+         *
+         * @access   protected
+         * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+         */
+        protected $plugin_name;
+
+        /**
+         * The version of this plugin.
+         *
+         * @access   private
+         * @var      string    $version    The current version of this plugin.
+         */
+        private $version;
+
+        /**
+         * Stores user session token.
+         * 
+         * @access private
+         * @var string $session_token The session token of user.
+         */
+        private $session_token;
+
+        /**
+         * Stores the status of login.
+         * 
+         * @access private
+         * @var string|bool $login_status The login status of user.
+         */
+        private $login_status = false;
+
+        /**
+         * Stores the blog id on which user is just logged in.
+         * @var int 
+         */
+        private $current_loggedin_blog_id;
+
+        /**
+         * Initialize the class and set its properties.
+         *
+         * @var      string    $plugin_name       The name of this plugin.
+         */
+        public function __construct($plugin_name, $version) {
+            $this->plugin_name = $plugin_name;
+            $this->version = $version;
+        }
+
+        /**
+         * Set the blog id on which user is just logged in.
+         */
+        public function set_current_loggedin_blog_id() {
+            $Session_Helper = new Faulh_Session_Helper($this->plugin_name);
+            $this->current_loggedin_blog_id = $Session_Helper->get_current_login_blog_id();
+        }
+
+        /**
+         * Blocks user if the user is not allowed to 
+         * login on another blog on the network.
+         * 
+         * @access private
+         */
+        private function is_blocked_user_on_current_blog($user_id) {
+            if(!is_multisite())
+            {
+                return FALSE;
+            }
+            
+            if(is_super_admin($user_id))
+            {
+                return FALSE; 
+            }
+            
+            if (!is_user_member_of_blog($user_id)) {
+                $Network_Admin_Setting = new Faulh_Network_Admin_Setting($this->plugin_name);
+                if ($Network_Admin_Setting->get_settings('block_user')) {
+                    $this->login_status = self::LOGIN_STATUS_BLOCK;
+                    $this->current_loggedin_blog_id = get_current_blog_id();
+                    wp_logout();
+                    wp_die($Network_Admin_Setting->get_settings('block_user_message'));
+                }
+            }
+        }
+
+        /**
+         * Saves user login details.
+         * 
+         * @access private
+         * @param string $user_login username
+         * @param object $user WP_User object
+         * @param string $status success, fail, logout, block etc.
+         */
+        private function save_login($user_login, $user, $status = '') {
+            if (empty($user_login)) {
+                return FALSE;
+            }
+
+            require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-faulh-browser-helper.php';
+            require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-faulh-geo-helper.php';
+            global $wpdb;
+            $unknown = 'unknown';
+            $table = $wpdb->get_blog_prefix() . FAULH_TABLE_NAME;
+            $current_date = Faulh_Date_Time_Helper::get_current_date_time();
+            $user_id = !empty($user->ID) ? $user->ID : FALSE;
+            $BrowserHelper = new Faulh_Browser_Helper();
+            $GeoHelper = new Faulh_Geo_Helper($this->plugin_name);
+            $geo_location = $GeoHelper->get_geo_location();
+            //now insert for new login
+            $data = array(
+                'user_id' => $user_id,
+                'session_token' => $this->get_session_token(),
+                'username' => $user_login,
+                'time_login' => $current_date,
+                'ip_address' => $GeoHelper->get_ip(),
+                'country_name' => !empty($geo_location['country_name']) ? $geo_location['country_name'] : $unknown,
+                'country_code' => !empty($geo_location['country_code']) ? $geo_location['country_code'] : $unknown,
+                'timezone' => !empty($geo_location['timezone']) ? $geo_location['timezone'] : $unknown,
+                'time_last_seen' => $current_date,
+                'browser' => $BrowserHelper->getBrowser(),
+                'browser_version' => $BrowserHelper->getVersion(),
+                'operating_system' => $BrowserHelper->getPlatform(),
+                'old_role' => !empty($user->roles) ? implode(",", $user->roles) : "",
+                'user_agent' => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : $unknown,
+                'login_status' => $status,
+                'is_super_admin' => is_multisite() ? is_super_admin($user_id) : FALSE,
+            );
+            //this is used to modify data before saving in db.
+            $filtered_data = apply_filters('faulh_before_save_login', $data);
+
+            if (is_array($filtered_data) && !empty($filtered_data)) {
+                $data = array_merge($data, $filtered_data);
+            }
+
+            $wpdb->insert($table, $data);
+
+            if ($wpdb->last_error || !$wpdb->insert_id) {
+                Faulh_Error_Handler::error_log("last error:" . $wpdb->last_error . " last query:" . $wpdb->last_query, __LINE__, __FILE__);
+                return;
+            }
+
+            do_action('faulh_after_save_login', $data);
+
+            if (self::LOGIN_STATUS_FAIL == $status) {
+                return;
+            }
+
+            $this->is_blocked_user_on_current_blog($user_id);
+        }
+
+        /**
+         * Fires if login success.
+         * @access  public
+         * @param string $user_login username
+         * @param object $user wp user object
+         */
+        public function user_login($user_login, $user) {
+            $this->save_login($user_login, $user, self::LOGIN_STATUS_LOGIN);
+        }
+
+        /**
+         * Update last seen time for the current user.
+         * 
+         * @access  public
+         * @global object $wpdb
+         * @return bool|int The number of records updated.
+         */
+        public function update_time_last_seen() {
+            global $wpdb;
+            $current_user = wp_get_current_user();
+            $table = $wpdb->get_blog_prefix($this->current_loggedin_blog_id) . FAULH_TABLE_NAME;
+            $current_date = Faulh_Date_Time_Helper::get_current_date_time();
+            $user_id = $current_user->ID;
+            $session_token = wp_get_session_token();
+
+            if (!$user_id) {
+                return;
+            }
+
+            $sql = "update $table set time_last_seen='$current_date' where session_token = '$session_token' and user_id = '$user_id' ";
+            $user_id = get_current_user_id();
+            $status = $wpdb->query($sql);
+
+            if ($wpdb->last_error) {
+                Faulh_Error_Handler::error_log("last error:" . $wpdb->last_error . " last query:" . $wpdb->last_query, __LINE__, __FILE__);
+            }
+
+            $data = array(
+                'time_last_seen' => $current_date,
+                'session_token' => $session_token,
+            );
+            do_action('faulh_update_time_last_seen', $data);
+        }
+
+        /**
+         * Fires if login failed.
+         * 
+         * @access public
+         * @param string $user_login username
+         */
+        public function user_login_failed($user_login) {
+            $this->save_login($user_login, NULL, self::LOGIN_STATUS_FAIL);
+        }
+
+        /**
+         * Fires on logout.
+         * Save logout time of current user.
+         * 
+         * @access public
+         */
+        public function user_logout() {
+            //use get_session_token just after login.
+            //use wp_get_session_token() after successful login redirect.
+            $session_token = wp_get_session_token() ? wp_get_session_token() : $this->get_session_token();
+            if (!$session_token) {
+                return;
+            }
+           
+            global $wpdb;
+            $time_logout = Faulh_Date_Time_Helper::get_current_date_time();
+            $login_status = $this->login_status ? $this->login_status : self::LOGIN_STATUS_LOGOUT;
+            $table = $wpdb->get_blog_prefix($this->current_loggedin_blog_id) . FAULH_TABLE_NAME;
+            $sql = "update $table  set time_logout='$time_logout', time_last_seen='$time_logout', login_status = '" . $login_status . "' where session_token = '" . $session_token . "' ";
+            
+            $wpdb->query($sql);
+
+            if ($wpdb->last_error) {
+                Faulh_Error_Handler::error_log("last error:" . $wpdb->last_error . " last query:" . $wpdb->last_query, __LINE__, __FILE__);
+            }
+
+            $data = array(
+                'time_logout' => $time_logout,
+                'time_last_seen' => $time_logout,
+                'session_token' => $session_token,
+            );
+            do_action('faulh_logout', $data);
+        }
+
+        /**
+         * Sets session token.
+         * 
+         * @param string $token The session token.
+         */
+        private function set_session_token($token) {
+            $this->session_token = $token;
+        }
+
+        /**
+         * Callback function for the action hook - set_logged_in_cookie
+         * 
+         * @param string $logged_in_cookie
+         * @param string $expire
+         * @param string $expiration
+         * @param string|int $user_id
+         * @param string $logged_in_text
+         * @param string $token The session token.
+         */
+        public function set_logged_in_cookie($logged_in_cookie, $expire, $expiration, $user_id, $logged_in_text, $token) {
+            $this->set_session_token($token);
+        }
+
+        /**
+         * Gets session token.
+         * 
+         * @return string The session token.
+         */
+        public function get_session_token() {
+            return $this->session_token ? $this->session_token : "";
+        }
+
+    }
