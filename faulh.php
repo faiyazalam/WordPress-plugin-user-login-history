c3fac7596348d2b83cebb2f01647d84eb5bd6d96
unknown <fyzalam@gmail.com>
2018-11-25 17:20:15 +0530

    deleted plugin table and options on uninstall event

--- faulh.php
+++ faulh.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The plugin bootstrap file
  *
@@ -27,34 +28,32 @@
 namespace User_Login_History;
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+    die;
 }
-
 /**
  * Define Constants
  */
+define(__NAMESPACE__ . '\NS', __NAMESPACE__ . '\\');
 
-define( __NAMESPACE__ . '\NS', __NAMESPACE__ . '\\' );
 
+define(NS . 'USER_LOGIN_HISTORY', 'faulh');
 
-define( NS . 'USER_LOGIN_HISTORY', 'faulh' );
+define(NS . 'PLUGIN_VERSION', '1.8.0');
 
-define( NS . 'PLUGIN_VERSION', '1.8.0' );
+define(NS . 'USER_LOGIN_HISTORY_DIR', plugin_dir_path(__FILE__));
 
-define( NS . 'USER_LOGIN_HISTORY_DIR', plugin_dir_path( __FILE__ ) );
+define(NS . 'USER_LOGIN_HISTORY_URL', plugin_dir_url(__FILE__));
 
-define( NS . 'USER_LOGIN_HISTORY_URL', plugin_dir_url( __FILE__ ) );
+define(NS . 'PLUGIN_BASENAME', plugin_basename(__FILE__));
 
-define( NS . 'PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-
-define( NS . 'PLUGIN_NAME', 'User Login History' );
-define( NS . 'PLUGIN_TEXT_DOMAIN', 'faulh' );
-define( NS . 'PLUGIN_OPTION_NAME_VERSION', 'fa_userloginhostory_version' );
-define( NS . 'PLUGIN_TABLE_FA_USER_LOGINS', 'fa_user_logins' );
-define( NS . 'DEFAULT_IS_STATUS_ONLINE_MIN', '2' );
-define( NS . 'DEFAULT_IS_STATUS_IDLE_MIN', '30' );
-define( NS . 'PLUGIN_BOOTSTRAP_FILE_PATH_FROM_PLUGIN_FOLDER', basename(__DIR__) . "/" . basename(__FILE__));
+define(NS . 'PLUGIN_NAME', 'User Login History');
+define(NS . 'PLUGIN_TEXT_DOMAIN', 'faulh');
+define(NS . 'PLUGIN_OPTION_NAME_VERSION', 'fa_userloginhostory_version');
+define(NS . 'PLUGIN_TABLE_FA_USER_LOGINS', 'fa_user_logins');
+define(NS . 'DEFAULT_IS_STATUS_ONLINE_MIN', '2');
+define(NS . 'DEFAULT_IS_STATUS_IDLE_MIN', '30');
+define(NS . 'PLUGIN_BOOTSTRAP_FILE_PATH_FROM_PLUGIN_FOLDER', basename(__DIR__) . "/" . basename(__FILE__));
 
 
 
@@ -64,23 +63,19 @@ define( NS . 'PLUGIN_BOOTSTRAP_FILE_PATH_FROM_PLUGIN_FOLDER', basename(__DIR__) 
 /**
  * Autoload Classes
  */
-
 require_once( USER_LOGIN_HISTORY_DIR . 'inc/libraries/autoloader.php' );
 
 /**
  * Register Activation and Deactivation Hooks
  * This action is documented in inc/core/class-activator.php
  */
-
-register_activation_hook( __FILE__, array( NS . 'Inc\Core\Activator', 'activate' ) );
+register_activation_hook(__FILE__, array(NS . 'Inc\Core\Activator', 'activate'));
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented inc/core/class-deactivator.php
  */
-
-register_deactivation_hook( __FILE__, array( NS . 'Inc\Core\Deactivator', 'deactivate' ) );
-
+register_deactivation_hook(__FILE__, array(NS . 'Inc\Core\Deactivator', 'deactivate'));
 
 /**
  * Plugin Singleton Container
@@ -91,27 +86,28 @@ register_deactivation_hook( __FILE__, array( NS . 'Inc\Core\Deactivator', 'deact
  */
 class User_Login_History {
 
-	/**
-	 * The instance of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @var      Init $init Instance of the plugin.
-	 */
-	private static $init;
-	/**
-	 * Loads the plugin
-	 *
-	 * @access    public
-	 */
-	public static function init() {
+    /**
+     * The instance of the plugin.
+     *
+     * @since    1.0.0
+     * @var      Init $init Instance of the plugin.
+     */
+    private static $init;
 
-		if ( null === self::$init ) {
-			self::$init = new Inc\Core\Init();
-			self::$init->run();
-		}
+    /**
+     * Loads the plugin
+     *
+     * @access    public
+     */
+    public static function init() {
 
-		return self::$init;
-	}
+        if (null === self::$init) {
+            self::$init = new Inc\Core\Init();
+            self::$init->run();
+        }
+
+        return self::$init;
+    }
 
 }
 
@@ -124,14 +120,14 @@ class User_Login_History {
  *
  * Also returns copy of the app object so 3rd party developers
  * can interact with the plugin's hooks contained within.
- **/
+ * */
 function wp_plugin_name_init() {
-		return User_Login_History::init();
+    return User_Login_History::init();
 }
 
 $min_php = '5.6.0';
 
 // Check the minimum required PHP version and run the plugin.
-if ( version_compare( PHP_VERSION, $min_php, '>=' ) ) {
-		wp_plugin_name_init();
+if (version_compare(PHP_VERSION, $min_php, '>=')) {
+    wp_plugin_name_init();
 }
--- uninstall.php
+++ uninstall.php
@@ -24,8 +24,89 @@
  *
  * @package    User_Login_History
  */
-
 // If uninstall not called from WordPress, then exit.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
 }
+
+
+if (!function_exists('faulh_delete_plugin_options')) {
+
+    function faulh_delete_plugin_options($prefix = '') {
+        if (empty($prefix)) {
+            return;
+        }
+        $options = array(
+            'fa_userloginhostory_version',
+            $prefix . "_basics",
+            $prefix . "_advanced",
+        );
+
+        foreach ($options as $option) {
+            delete_option($option);
+        }
+    }
+
+}
+
+
+
+if (!function_exists('faulh_uninstall_plugin')) {
+
+    function faulh_uninstall_plugin() {
+        $plugin_name = 'faulh';
+        $table_name = 'fa_user_logins';
+
+        global $wpdb;
+        if (is_multisite()) {
+            $blog_ids = faulh_get_blogs_of_current_network();
+            foreach ($blog_ids as $blog_id) {
+                switch_to_blog($blog_id);
+                $wpdb->query("DROP TABLE IF EXISTS " . $wpdb->prefix . $table_name);
+                faulh_delete_plugin_options($plugin_name);
+            }
+            restore_current_blog();
+        } else {
+            $wpdb->query("DROP TABLE " . $wpdb->prefix . $table_name);
+            faulh_delete_plugin_options($plugin_name);
+        }
+
+        $user_meta_keys = array(
+            $plugin_name . "_timezone",
+            $plugin_name . "_rows_per_page",
+            "managetoplevel_page_" . $plugin_name . "-login-listingcolumnshidden",
+        );
+        $sql_in = "'" . implode("', '", $user_meta_keys) . "'";
+
+        $wpdb->query("DELETE FROM $wpdb->usermeta WHERE meta_key IN ($sql_in)");
+    }
+
+}
+
+
+if (!function_exists('faulh_get_blogs_of_current_network')) {
+
+    function faulh_get_blogs_of_current_network() {
+        global $wpdb;
+        $sql = "SELECT blog_id FROM $wpdb->blogs WHERE site_id = " . get_current_network_id() . "  ORDER BY blog_id ASC";
+        $result = $wpdb->get_col($sql);
+        if ($wpdb->last_error) {
+            faulh_error_log("last error:" . $wpdb->last_error . " last query:" . $wpdb->last_query);
+        }
+        return $result;
+    }
+
+}
+
+if (!function_exists('faulh_error_log')) {
+
+    function faulh_error_log($message = '') {
+        ini_set('error_log', WP_CONTENT_DIR . '/user-login-history.log');
+        error_log("Error While Uninstalling: " . $message);
+    }
+
+}
+
+
+
+faulh_uninstall_plugin();
