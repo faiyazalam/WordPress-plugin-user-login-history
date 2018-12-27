ba506751c2d6e8f35e1e7d58fcf58f6074092bcd
yasmin <fyzalam@gmail.com>
2018-12-27 17:59:28 +0530

    set inlice css

--- inc/admin/views/settings/network-admin.php
+++ inc/admin/views/settings/network-admin.php
@@ -1,6 +1,6 @@
 <?php
-use User_Login_History\Inc\Common\Helpers\Template as Template_Helper;
 
+use User_Login_History\Inc\Common\Helpers\Template as Template_Helper;
 ?>
 <div class="wrap faulhSettingPage">
     <?php echo Template_Helper::head(esc_html__('Network Settings', 'faulh')) ?>
@@ -10,19 +10,19 @@ use User_Login_History\Inc\Common\Helpers\Template as Template_Helper;
             <div class="mt20">
                 <label class="infoBlockUser"> <strong><?php esc_html_e('Block User', 'faulh') ?></strong></label>
                 <label for="block_user">
-                    <input id="block_user" type="checkbox" <?php checked($this->get_settings('block_user'), 1); ?>  name="block_user" value="<?php echo esc_attr($this->get_settings('block_user')); ?>" size="50" />
+                    <input style="margin-left: 134px" id="block_user" type="checkbox" <?php checked($this->get_settings('block_user'), 1); ?>  name="block_user" value="<?php echo esc_attr($this->get_settings('block_user')); ?>" size="50" />
                     <?php esc_html_e('User will not be able to login on another blog on the network.', 'faulh'); ?>
                 </label>   
             </div>
             <div class="mt20">&nbsp;</div>
             <div class="mt20">
-                <label for="block_user_message">
+                <label for="block_user_message" style="float: left">
                     <strong><?php esc_html_e('Message for Blocked User', 'faulh'); ?></strong>
                 </label>
-                <textarea id="block_user_message" name="block_user_message"><?php echo esc_attr($this->get_settings('block_user_message')); ?></textarea>
+                <textarea  style="float: right; margin-right: 693px" id="block_user_message" name="block_user_message"><?php echo esc_attr($this->get_settings('block_user_message')); ?></textarea>
             </div>
         </fieldset>
-         
+
         <?php wp_nonce_field($this->get_form_nonce_name(), $this->get_form_nonce_name()); ?>
         <?php submit_button(); ?>
     </form>
