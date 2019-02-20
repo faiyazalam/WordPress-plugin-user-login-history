3242b8259acd56afc5abde34e16e66f6530739c9
faiyaz <faiyaz.alam@happiestminds.com>
2019-02-20 16:42:51 +0530

    added calendar icon on fron-end listing table

--- inc/frontend/views/forms/filter.php
+++ inc/frontend/views/forms/filter.php
@@ -5,8 +5,8 @@ use User_Login_History\Inc\Common\Helpers\Template as Template_Helper;
 <form name="<?php echo $this->plugin_name . '-search-form'; ?>" method="get" action="" id="<?php echo $this->plugin_name . '-search-form'; ?>">
     <fieldset> 
 
-        <input style="width:39%;display: inline-block" readonly type="text" autocomplete="off" placeholder="<?php esc_html_e("From", "faulh") ?>" id="date_from" name="date_from" value="<?php echo isset($_GET['date_from']) ? esc_attr($_GET['date_from']) : "" ?>" >
-        <input style="width:39%;display: inline-block" readonly type="text" autocomplete="off" placeholder="<?php esc_html_e("To", "faulh") ?>" name="date_to" id="date_to" value="<?php echo isset($_GET['date_to']) ? esc_attr($_GET['date_to']) : "" ?>" >
+        <input class="date-inputss" style="width:39%;display: inline-block" readonly type="text" autocomplete="off" placeholder="<?php esc_html_e("From", "faulh") ?>" id="date_from" name="date_from" value="<?php echo isset($_GET['date_from']) ? esc_attr($_GET['date_from']) : "" ?>" >
+        <input class="date-inputss" style="width:39%;display: inline-block" readonly type="text" autocomplete="off" placeholder="<?php esc_html_e("To", "faulh") ?>" name="date_to" id="date_to" value="<?php echo isset($_GET['date_to']) ? esc_attr($_GET['date_to']) : "" ?>" >
         <select name="date_type" >
             <?php
             Template_Helper::dropdown_time_field_types(isset($_GET['date_type']) ? $_GET['date_type'] : NULL);
